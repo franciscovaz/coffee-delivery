8f127c40cfeead1cb37a350ac92eecfbb74b1b59
franciscovaz <ftomasvaz@gmail.com>
2025-03-08 10:01:41 +0000

    - add styled components; - add default theme and global configs; - add app logo; - add types for default theme;

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <p>Lets start Coffee Delivery app</p>
-    </>
+    </ThemeProvider>
   )
 }
 
