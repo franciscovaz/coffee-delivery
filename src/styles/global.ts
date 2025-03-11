4be202d8b1e0ef9c2165ec8026b7adc552eee6b0
franciscovaz <ftomasvaz@gmail.com>
2025-03-11 21:45:26 +0000

    - update global styles;

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,26 +1,25 @@
 import { createGlobalStyle } from 'styled-components'
+import { mixins } from './mixins'
 
 export const GlobalStyle = createGlobalStyle`
-    * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
-    :focus {
-        outline: 0;
-        /* box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']}; */
-    }
+  body {
+    background: ${(props) => props.theme.background};
+    color: ${(props) => props.theme['base-text']};
+    -webkit-font-smoothing: antialiased;
+  }
 
-    body {
-        background: ${(props) => props.theme['background']};
-        color: ${(props) => props.theme['base-text']};
-        -webkit-font-smoothing: antialiased;
-    }
+  body, input, textarea, button {
+    ${mixins.fonts.textM};
+  }
 
-    body, input, textarea, button {
-        font-family: 'Baloo 2', sans-serif;
-        font-weight: 400;
-        font-size: 1rem;
-    }
+  button {
+    border: none;
+    cursor: pointer;
+  }
 `
