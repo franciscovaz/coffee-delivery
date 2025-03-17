13a4344ad6dd9c7f503c7e9f8d70e0c7b9db6de6
franciscovaz <ftomasvaz@gmail.com>
2025-03-17 22:47:46 +0000

    - add immer to improve reducer logic; - add useCart hook to help using context; - added content to cart context provider; - added content to reducer;

--- src/contexts/CartContextProvider.tsx
+++ src/contexts/CartContextProvider.tsx
@@ -1,6 +1,14 @@
-import { createContext, ReactNode } from 'react'
+import { createContext, ReactNode, useEffect, useReducer } from 'react'
 import { OrderInfo } from '../pages/Cart'
-import { Item, Order } from '../store/cart/reducer'
+import { Item, Order, CartReducer } from '../store/cart/reducer'
+import { useNavigate } from 'react-router-dom'
+import {
+  addItemAction,
+  removeItemAction,
+  checkoutCartAction,
+  incrementItemQuantityAction,
+  decrementItemQuantityAction,
+} from '../store/cart/actions'
 
 interface CartContextType {
   cart: Item[]
@@ -18,4 +26,70 @@ interface CartContextProviderProps {
   children: ReactNode
 }
 
-export function CartContextProvider({ children }: CartContextProviderProps) {}
+export function CartContextProvider({ children }: CartContextProviderProps) {
+  const [cartState, dispatch] = useReducer(
+    CartReducer,
+    {
+      cart: [],
+      orders: [],
+    },
+    (cartState) => {
+      const storedStateAsJSON = localStorage.getItem(
+        '@coffee-delivery:cart-state-1.0.0',
+      )
+
+      if (storedStateAsJSON) {
+        return JSON.parse(storedStateAsJSON)
+      }
+
+      return cartState
+    },
+  )
+  const navigate = useNavigate()
+
+  const { cart, orders } = cartState
+
+  function addItem(item: Item) {
+    dispatch(addItemAction(item))
+  }
+
+  function removeItem(itemId: Item['id']) {
+    dispatch(removeItemAction(itemId))
+  }
+
+  function checkout(order: OrderInfo) {
+    dispatch(checkoutCartAction(order, navigate))
+  }
+
+  function incrementItemQuantity(itemId: Item['id']) {
+    dispatch(incrementItemQuantityAction(itemId))
+  }
+
+  function decrementItemQuantity(itemId: Item['id']) {
+    dispatch(decrementItemQuantityAction(itemId))
+  }
+
+  useEffect(() => {
+    if (cartState) {
+      const stateJSON = JSON.stringify(cartState)
+
+      localStorage.setItem('@coffee-delivery:cart-state-1.0.0', stateJSON)
+    }
+  }, [cartState])
+
+  return (
+    <CartContext.Provider
+      value={{
+        addItem,
+        cart,
+        orders,
+        decrementItemQuantity,
+        incrementItemQuantity,
+        removeItem,
+        checkout,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  )
+}
